76b291a4c88e7e6a077af1cd8ff0e1cdb76c997c
Tri Nguyen <tri@tridnguyen.com>
2015-07-08 15:25:45 -0400

    use better console

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 var assign = require('lodash.assign');
 var caller = require('caller');
 var path = require('path');
+var console = require('better-console');
 
 module.exports = function (configPath, defaults) {
 	var config = defaults || {};
 	try {
 		config = assign(config, require(path.resolve(path.dirname(caller()), configPath)));
 	} catch (e) {
+		console.error(e);
 	}
 	return config;
 };
