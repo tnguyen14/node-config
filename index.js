3819f02cc86eee8db6163a9612714e06e8f927c3
Tri Nguyen <tri@tridnguyen.com>
2015-07-08 15:24:41 -0400

    use caller to resolve path correctly

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var assign = require('lodash.assign');
+var caller = require('caller');
+var path = require('path');
+
 module.exports = function (configPath, defaults) {
 	var config = defaults || {};
-	try  {
-		config = assign(config, require(configPath));
+	try {
+		config = assign(config, require(path.resolve(path.dirname(caller()), configPath)));
 	} catch (e) {
 	}
 	return config;
-}
-
+};
