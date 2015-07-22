17f1275b546a2f0a23f541b78d9f770965ddc9fc
Tri Nguyen <tri@tridnguyen.com>
2015-07-21 22:20:39 -0400

    defaults as first parameter, as option to not resolve path with caller

--- README.md
+++ README.md
@@ -10,11 +10,16 @@
 ## Usage
 
 ```js
-var config = require('tridnguyen/config');
+var config = require('@tridnguyen/config');
 
-var options = config('./config.js', {
+var options = config({
 	a: 'default',
 	b: 'value'
-});
+}, 'config.json');
 ```
+### Options
+**`config(defaults, configPath, opts)`**
 
+- **defaults** {Object} - default options for config. **Defaults to `{}`**
+- **configPath** {String} - the path to the config file. It could be either a JSON or JS file
+- **opts.caller** {Boolean} - whether the config file path is relative to the script that calls `config`. **Defaults to `true`**
--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 'use strict';
 
 var assign = require('lodash.assign');
+var isString = require('lodash.isstring');
+var isObject = require('lodash.isobject');
 var caller = require('caller');
 var path = require('path');
 var console = require('better-console');
 
-module.exports = function (configPath, defaults) {
-	var config = defaults || {};
+module.exports = function (defaults, configPath, opts) {
+	var _configPath = configPath;
+	var _opts = {
+		caller: true
+	};
+	var config = {};
+
+	if (isObject(defaults)) {
+		config = defaults;
+	}
+	if (isObject(opts)) {
+		assign(_opts, opts);
+	}
+	// if there's only one argument
+	if (arguments.length === 1) {
+		// if it is an object, return it right away
+		if (isObject(defaults)) {
+			return config;
+		// if it is a string, consider it file path
+		} else if (isString(defaults)) {
+			_configPath = defaults;
+		}
+	} else if (arguments.length === 2) {
+		// pass in configPath and opts only
+		if (isString(defaults)) {
+			_configPath = defaults;
+			assign(_opts, configPath);
+		// if pass in 2 objects, bail early
+		} else if (isObject(defaults) && !isString(configPath)) {
+			return config;
+		}
+	}
+
+	if (_opts.caller) {
+		_configPath = path.resolve(path.dirname(caller()), _configPath);
+	}
+
 	try {
-		config = assign(config, require(path.resolve(path.dirname(caller()), configPath)));
+		config = assign(config, require(_configPath));
 	} catch (e) {
 		console.error(e);
 	}
